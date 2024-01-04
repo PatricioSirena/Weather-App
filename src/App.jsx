9fc613df6731601e1e1527eff4f700db869d8ed2
Patricio Sirena <patobsirena@gmail.com>
2024-01-04 10:48:01 -0300

    Agrego key para api de clima

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
 
-
+console.log(import.meta.env.VITE_API_KEY);
 function App() {
-
 
   return (
     <>
-      
+    <div className="titulo">
+      <h1>Weather App</h1>
+    </div>
+    <Container>
+
+    </Container>
     </>
   )
 }
